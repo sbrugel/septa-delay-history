6ea2e03d46c3730756b82075c11624f7f3bab3c6
sbrugel <sbrugel62@gmail.com>
2022-10-13 18:56:30 -0400

    feat: database population script

--- tracker/src/index.ts
+++ tracker/src/index.ts
@@ -1,26 +1,55 @@
 import axios from 'axios';
 import cron from 'cron';
+import { Db, MongoClient } from 'mongodb';
+import { DB } from './config';
 
 async function main() {
-    const job = new cron.CronJob('*/15 * * * *', fetchDelays);
+    console.log('Connecting to db...');
+    let mongo: Db;
+    await MongoClient.connect(DB.CONNECTION).then((bot) => {
+		mongo = bot.db(DB.COLLECTION);
+	});
+    console.log('Connected');
+
+    console.log('Preparing to track...');
+    const job = new cron.CronJob('*/15 * * * *', () => fetchDelays(mongo));
     job.start();
 
     console.log('Now tracking');
+    fetchDelays(mongo);
 }
 
-async function fetchDelays() {
-    const delays: Delay[] = [];
+async function fetchDelays(mongo: Db) {
     await axios.get("http://www3.septa.org/hackathon/TrainView/")
-    .then((res) => {
+    .then(async (res) => {
         const data = res.data;
         const d = new Date();
 
         for (const train of data) {
-            delays.push({
+            const targetService = await mongo.collection(DB.DATA).findOne({ service: train.trainno });
+            const recentDelays = targetService ? targetService.delays : [];
+            recentDelays.push({
                 amount: train.late,
                 datestring: d,
                 date: d.toLocaleDateString("en-US")
             });
+            if (recentDelays.length > 3000) {
+                recentDelays.shift();
+            }
+
+            if (!targetService) {
+                await mongo.collection(DB.DATA).insertOne({
+                    service: train.trainno,
+                    delays: recentDelays
+                });
+            } else {
+                await mongo.collection(DB.DATA).findOneAndUpdate(
+                    { service: train.trainno },
+                    { $set: {
+                        delays: recentDelays
+                    }}
+                );
+            }
         }
     });
 }
