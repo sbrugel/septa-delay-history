384df03dbdd68b07697d1216948d0a3e6aa30879
Simon Brugel <sbrugel62@gmail.com>
2022-10-14 13:22:11 -0400

    server connection complete (untested with react)

---
+++ server/db/conn.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+let _db;
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) { // I don't care this is deprecated I just want the code to work ;(
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("delays");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
---
+++ server/routes/train.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const trainRoutes = express.Router();
+ 
+// connect to database
+const dbo = require("../db/conn");
+ 
+// convert id string to ObjectID
+const ObjectId = require("mongodb").ObjectId;
+ 
+// get all trains
+trainRoutes.route("/train").get(function (req, res) {
+ let db_connect = dbo.getDb("employees");
+ db_connect
+   .collection("delayDB")
+   .find({})
+   .toArray(function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+ 
+// get a single train by ID
+trainRoutes.route("/train/:id").get(function (req, res) {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ db_connect
+   .collection("delayDB")
+   .findOne(myquery, function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+ 
+module.exports = trainRoutes;
